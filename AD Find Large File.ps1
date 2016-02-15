2de2e82b14f299ec0209dd0934482fbf7a89ec21
rhi-ko <kamaleon204@yahoo.com>
2016-02-14 21:23:11 -0600

    Update AD Find Large File.ps1

--- AD Find Large File.ps1
+++ AD Find Large File.ps1
@@ -1,8 +1,8 @@
 $User = Read-host -Prompt 'Enter Username'
-$Serverq = Read-Host -Prompt 'Enter Server Name'
-$Session = New-PSSession -computername $Serverq -credential domain01\$User
-$runme = gci -path 'f:\' -r | sort Length -desc | select-object @{Name='SizeGB';Expression={ "{0:N2}" -f ($_.Length / 1GB) }},fullname -f 20
+$Machinename = Read-Host -Prompt 'Enter Server Name'
+$Session = New-PSSession -computername $Machinename -credential domain01\$User
+$Runme = gci -path 'f:\' -r | sort Length -desc | select-object @{Name='SizeGB';Expression={ "{0:N2}" -f ($_.Length / 1GB) }},fullname -f 20
 
-Invoke-command -session $session -scriptblock $Runme
+Invoke-command -session $Session -scriptblock $Runme
 
 Exit-PSSession
