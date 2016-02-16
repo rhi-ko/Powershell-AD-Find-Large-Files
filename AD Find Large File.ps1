b94a860160dba7dcbf9b3746c219c47bd1eff110
rhi-ko <kamaleon204@yahoo.com>
2016-02-15 21:57:01 -0600

    Update AD Find Large File.ps1

--- AD Find Large File.ps1
+++ AD Find Large File.ps1
@@ -1,7 +1,13 @@
-new-pssession -computername $c 
-
 $c = read-host -Prompt 'Name of server'
+$job = start-job -scriptblock { gci -path 'c:\' -r  | sort Length -desc | select @{Name='SizeGB';Expression={ "{0:N3}" -f ($_.Length / 1GB) }},@{Name='Last Modified Date';E={$_.LastWriteTime.ToShortDateString()}},fullname -f 20 } -Credential $cred
+$job | Wait-job
+Receive-Job $job 
+Get-Job | Remove-Job 
 
-Invoke-Command -Computername $c -Credential $cred -ScriptBlock { gci -path 'c:\' -r  | sort Length -desc | select @{Name='SizeGB';Expression={ "{0:N2}" -f ($_.Length / 1GB) }},fullname -f 20 | out-string}
+new-pssession -computername $c | Enter-pssession
+
+
+#Invoke-Command -Computername $c -ScriptBlock { gci -path 'c:\' -r  | sort Length -desc |`
+# select @{Name='SizeGB';Expression={ "{0:N2}" -f ($_.Length / 1GB) }},fullname -f 20 | out-string} -Credential $cred #-asjob
 
 Get-pssession -Computername $c | Remove-Pssession
